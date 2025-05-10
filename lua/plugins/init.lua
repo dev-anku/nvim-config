e7aa02d4648e205143a7f3b15bab42371f7eb73f
dev-anku <ankitkumarmuhal3@gmail.com>
2025-05-11 02:15:29 +0530

    Base Neovim Config

---
+++ lua/plugins/init.lua
@@ -0,0 +1,11 @@
+return {
+
+    {
+        "nvim-lua/plenary.nvim",
+        name = "plenary"
+    },
+
+    "eandrju/cellular-automaton.nvim",
+    
+    { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+}
